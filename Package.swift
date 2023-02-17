ccbe3737552ecc661b8f9fff8d475f04f507a057
Build Er <builder@macmini6.local>
2023-02-16 23:51:09 -0300

    Package version: 1.0.0-beta+20230217024255

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXDataLayerLocal",
+	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "GXDataLayerLocal",
+			targets: ["GXDataLayerLocalWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", branch: "beta")
+	],
+	targets: [
+		.target(name: "GXDataLayerLocalWrapper",
+				dependencies: [
+					"GXDataLayerLocal",
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.watchOS, .tvOS, .iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXDataLayerLocal",
+			url: "https://pkgs.genexus.dev/iOS/beta/GXDataLayerLocal-1.0.0-beta+20230217024255.xcframework.zip",
+			checksum: "ce447aa977f571469939fd56c41dffdb729d551e35546855be806d35f1e6dad6"
+		)
+	]
+)
